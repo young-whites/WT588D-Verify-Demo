70303b1086696ee0c532da28e5975a441e22c28b
Mr.Nights <2863692760@qq.com>
2025-10-20 03:35:22 +0800

    2025-10-20

--- Verify_WT588D-Demo/Debug/libraries/CMSIS/Device/ST/STM32F1xx/Source/Templates/gcc/subdir.mk
+++ Verify_WT588D-Demo/Debug/libraries/CMSIS/Device/ST/STM32F1xx/Source/Templates/gcc/subdir.mk
@@ -15,5 +15,5 @@ S_UPPER_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 libraries/CMSIS/Device/ST/STM32F1xx/Source/Templates/gcc/%.o: ../libraries/CMSIS/Device/ST/STM32F1xx/Source/Templates/gcc/%.S
-	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -O0 -ffunction-sections -fdata-sections -Wall  -g -gdwarf-2 -x assembler-with-cpp -I"C:\Users\18452\Documents\Github-young-nights\wt588d-verify-demo\Verify_WT588D-Demo" -Xassembler -mimplicit-it=thumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -O0 -ffunction-sections -fdata-sections -Wall  -g -gdwarf-2 -x assembler-with-cpp -I"I:\GitHub-young-nights\wt588d-verify-demo\Verify_WT588D-Demo" -Xassembler -mimplicit-it=thumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
 
